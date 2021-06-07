da6436c2b8cbc1fc9ee341670adb082231ef09b6
Simon Wilson <contact@simonwilson.design>
2021-06-07 14:51:15 +0100

    Initial commit

---
+++ studio/schemas/schema.js
@@ -0,0 +1,28 @@
+import createSchema from "part:@sanity/base/schema-creator"
+import schemaTypes from "all:part:@sanity/base/schema-type"
+
+import seo from "./documents/seo"
+
+import about from "./documents/about"
+import contact from "./objects/about/contact"
+import social from "./objects/about/social";
+
+import content from "./documents/content"
+import thumbnail from "./objects/project/thumbnail"
+import layout from "./objects/project/layout"
+
+
+export default createSchema({
+    name: "mySchema",
+    types: schemaTypes.concat([
+        seo,
+
+        about,
+        contact,
+        social,
+
+        content,
+        thumbnail,
+        layout
+    ]),
+});
---
+++ web/gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteMetadata: {
+    title: "Anna Gerber",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "7na0qgn7",
+        dataset: "production",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
