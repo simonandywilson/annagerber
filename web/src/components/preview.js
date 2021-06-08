8ea4d04ea1aff5602cbfba6313d85cbf43176ced
Simon Wilson <contact@simonwilson.design>
2021-06-08 22:17:55 +0100

    Added video

--- web/src/components/preview.js
+++ web/src/components/preview.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 import { getGatsbyImageData } from "gatsby-source-sanity";
+import ReactPlayer from "react-player";
 import * as style from "../styles/preview.module.css";
 
 const Preview = (props) => {
@@ -12,10 +13,19 @@ const Preview = (props) => {
         <div className={style.preview}>
             <div className={style.wrapper} style={{ opacity: props.preview.active ? "1" : "0" }}>
                 {props.preview.image && (
-                    <GatsbyImage
-                        image={imageData}
-                        alt=""
-                        imgStyle={{ objectFit: "contain" }}
+                    <GatsbyImage image={imageData} alt="" imgStyle={{ objectFit: "contain" }} />
+                )}
+                {props.preview.video && (
+                    <ReactPlayer
+                        url={props.preview.video}
+                        width="100%"
+                        height="100%"
+                        playsinline={true}
+                        muted={true}
+                        autoplay={true}
+                        controls={false}
+                        loop={true}
+                        playing={true}
                     />
                 )}
             </div>
